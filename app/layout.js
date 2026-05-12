caa19ae002aff568e1131fd2da1b4cc5bad43310
Surahan Choi <surahanchoi@SurahanMac.local>
2026-05-12 23:06:42 +0800

    fix: noscript 태그 hydration 오류 수정 — dangerouslySetInnerHTML 사용

--- app/layout.js
+++ app/layout.js
@@ -50,15 +50,7 @@ export default function RootLayout({ children }) {
               fbq('init', '${pixelId}');
               fbq('track', 'PageView');
             `}</Script>
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-                alt=""
-              />
-            </noscript>
+            <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1" alt="" />` }} />
           </>
         )}
       </body>
